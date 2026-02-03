d609c0fb25eb4c49a9e2e65002db68035effcf5a
zurakotaro-24 <calingjared0224@gmail.com>
2026-02-03 09:11:21 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,41 @@
-import react from "@vitejs/plugin-react"
-import * as path from "node:path"
-import { defineConfig } from "vitest/config"
-import packageJson from "./package.json" with { type: "json" }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    open: true,
-  },
-
-  test: {
-    root: import.meta.dirname,
-    name: packageJson.name,
-    environment: "jsdom",
-
-    typecheck: {
-      enabled: true,
-      tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
-    },
-
-    globals: true,
-    watch: false,
-    setupFiles: ["./src/setupTests.ts"],
+  base: '/',
+  build: {
+    outDir: 'dist'
   },
 })
+
+
+// import react from "@vitejs/plugin-react"
+// import * as path from "node:path"
+// import { defineConfig } from "vitest/config"
+// import packageJson from "./package.json" with { type: "json" }
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+//   server: {
+//     open: true,
+//   },
+
+//   test: {
+//     root: import.meta.dirname,
+//     name: packageJson.name,
+//     environment: "jsdom",
+
+//     typecheck: {
+//       enabled: true,
+//       tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+//     },
+
+//     globals: true,
+//     watch: false,
+//     setupFiles: ["./src/setupTests.ts"],
+//   },
+// })
